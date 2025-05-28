5200293703377d3048a7b1ecad152302a8fe984a
Letícia Teles <levteles@hotmail.com>
2025-05-28 13:24:50 -0300

    atualizando

---
+++ src/main/java/org/serratec/projetofinal_api_g4/controller/CategoriaController.java
@@ -0,0 +1,7 @@
+package org.serratec.projetofinal_api_g4.controller;
+
+
+
+public class CategoriaController {
+
+}
---
+++ src/main/java/org/serratec/projetofinal_api_g4/dto/CategoriaDTO.java
@@ -0,0 +1,29 @@
+package org.serratec.projetofinal_api_g4.dto;
+
+import org.serratec.projetofinal_api_g4.domain.Categoria;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor 
+@AllArgsConstructor 
+public class CategoriaDTO {
+
+  @Id
+  private Long id;
+
+  private String nome;
+  
+  private String descricao;
+
+ 
+
+  public CategoriaDTO(Categoria categoria) {
+    //TODO Auto-generated constructor stub
+  }
+    
+
+}
