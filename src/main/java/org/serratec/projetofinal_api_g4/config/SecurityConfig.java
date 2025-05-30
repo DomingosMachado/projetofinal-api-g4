30c9c1fd005c1105eb0a8861831b06ebc6ec3c6f
Letícia Teles <levteles@hotmail.com>
2025-05-30 20:45:15 -0300

    puxando para nova branch

--- src/main/java/org/serratec/projetofinal_api_g4/config/SecurityConfig.java
+++ src/main/java/org/serratec/projetofinal_api_g4/config/SecurityConfig.java
@@ -12,6 +12,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import jakarta.servlet.Filter;
 
 
 @Configuration
@@ -57,7 +60,7 @@ public class SecurityConfig {
             );
             
         // Comentado para permitir acesso sem autenticação durante desenvolvimento
-         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+       // http.addFilterBefore(jwtAuthenticationFilter, (Class<? extends Filter>) UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
